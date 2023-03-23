40b7d842bff17c9c5d0cae789564c4519e823c06
markortleb <markortleb@gmail.com>
2023-03-23 16:38:56 -0600

    adding some css

--- src/components/form/EducationSection.js
+++ src/components/form/EducationSection.js
@@ -18,12 +18,14 @@ class EducationSection extends Component {
         return (
             <fieldset>
                 <legend>Education</legend>
-                <input type="text" placeholder="University" value={education.university} onChange={ e => this.handleInputChange(e, 'university')}/>
-                <input type="text" placeholder="Location" value={education.location} onChange={ e => this.handleInputChange(e, 'location')}/>
-                <input type="text" placeholder="Degree" value={education.degree} onChange={ e => this.handleInputChange(e, 'degree')}/>
-                <input type="text" placeholder="Subject" value={education.subject} onChange={ e => this.handleInputChange(e, 'subject')}/>
-                <input type="text" placeholder="From" value={education.from} onChange={ e => this.handleInputChange(e, 'from')}/>
-                <input type="text" placeholder="To" value={education.to} onChange={ e => this.handleInputChange(e, 'to')}/>
+                <div className="inputArea">
+                    <input type="text" placeholder="University" value={education.university} onChange={ e => this.handleInputChange(e, 'university')}/>
+                    <input type="text" placeholder="Location" value={education.location} onChange={ e => this.handleInputChange(e, 'location')}/>
+                    <input type="text" placeholder="Degree" value={education.degree} onChange={ e => this.handleInputChange(e, 'degree')}/>
+                    <input type="text" placeholder="Subject" value={education.subject} onChange={ e => this.handleInputChange(e, 'subject')}/>
+                    <input type="text" placeholder="From" value={education.from} onChange={ e => this.handleInputChange(e, 'from')}/>
+                    <input type="text" placeholder="To" value={education.to} onChange={ e => this.handleInputChange(e, 'to')}/>
+                </div>
                 <button>Delete</button>
                 <button>Add</button>
             </fieldset>
--- src/components/form/PersonalInfoSection.js
+++ src/components/form/PersonalInfoSection.js
@@ -19,14 +19,16 @@ class PersonalInfoSection extends Component {
         return (
             <fieldset>
                 <legend>Personal Information</legend>
-                <input type="text" placeholder="First Name" value={personalInfo.firstName} onChange={ e => this.handleInputChange(e, 'firstName')}/>
-                <input type="text" placeholder="Last Name" value={personalInfo.lastName} onChange={ e => this.handleInputChange(e, 'lastName')}/>
-                <input type="text" placeholder="Title" value={personalInfo.title} onChange={ e => this.handleInputChange(e, 'title')}/>
-                <input type="text" placeholder="Location" value={personalInfo.location} onChange={ e => this.handleInputChange(e, 'location')}/>
-                <input type="tel" placeholder="Phone Number" value={personalInfo.phoneNumber} onChange={ e => this.handleInputChange(e, 'phone')}/>
-                <input type="email" placeholder="Email" value={personalInfo.email} onChange={ e => this.handleInputChange(e, 'email')}/>
-                <input type="text" placeholder="Website" value={personalInfo.website} onChange={ e => this.handleInputChange(e, 'website')}/>
-                <input type="text" placeholder="Description" value={personalInfo.description} onChange={ e => this.handleInputChange(e, 'description')}/>
+                <div className="inputArea">
+                    <input type="text" placeholder="First Name" value={personalInfo.firstName} onChange={ e => this.handleInputChange(e, 'firstName')}/>
+                    <input type="text" placeholder="Last Name" value={personalInfo.lastName} onChange={ e => this.handleInputChange(e, 'lastName')}/>
+                    <input type="text" placeholder="Title" value={personalInfo.title} onChange={ e => this.handleInputChange(e, 'title')}/>
+                    <input type="text" placeholder="Location" value={personalInfo.location} onChange={ e => this.handleInputChange(e, 'location')}/>
+                    <input type="tel" placeholder="Phone Number" value={personalInfo.phoneNumber} onChange={ e => this.handleInputChange(e, 'phone')}/>
+                    <input type="email" placeholder="Email" value={personalInfo.email} onChange={ e => this.handleInputChange(e, 'email')}/>
+                    <input type="text" placeholder="Website" value={personalInfo.website} onChange={ e => this.handleInputChange(e, 'website')}/>
+                    <input type="text" placeholder="Description" value={personalInfo.description} onChange={ e => this.handleInputChange(e, 'description')}/>
+                </div>
             </fieldset>
         )
     }
