0265fa8a0491633ea4de1f2caba7940cc02da2ac
markortleb <markortleb@gmail.com>
2023-03-25 13:26:18 -0600

    fix typo

--- src/components/form/PersonalInfoSection.js
+++ src/components/form/PersonalInfoSection.js
@@ -24,7 +24,7 @@ class PersonalInfoSection extends Component {
                     <input type="text" placeholder="Last Name" value={personalInfo.lastName} onChange={ e => this.handleInputChange(e, 'lastName')}/>
                     <input type="text" placeholder="Title" value={personalInfo.title} onChange={ e => this.handleInputChange(e, 'title')}/>
                     <input type="text" placeholder="Location" value={personalInfo.location} onChange={ e => this.handleInputChange(e, 'location')}/>
-                    <input type="tel" placeholder="Phone Number" value={personalInfo.phoneNumber} onChange={ e => this.handleInputChange(e, 'phone')}/>
+                    <input type="tel" placeholder="Phone Number" value={personalInfo.phoneNumber} onChange={ e => this.handleInputChange(e, 'phoneNumber')}/>
                     <input type="email" placeholder="Email" value={personalInfo.email} onChange={ e => this.handleInputChange(e, 'email')}/>
                     <input type="text" placeholder="Website" value={personalInfo.website} onChange={ e => this.handleInputChange(e, 'website')}/>
                     <input type="text" placeholder="Description" value={personalInfo.description} onChange={ e => this.handleInputChange(e, 'description')}/>
